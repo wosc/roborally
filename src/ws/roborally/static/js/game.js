2e97d006b4ebd53e6dee41cedad8a44b96446d59
Wolfgang Schnerring <wosc@wosc.de>
2020-10-24 14:38:54 +0200

    Implement locking slots due to damage

--- src/ws/roborally/static/js/game.js
+++ src/ws/roborally/static/js/game.js
@@ -34,7 +34,11 @@ const GameController = {
                 return response.json();
             }).then(function(data) {
                 self.data.draw = data['cards'];
-                self.data.turn = [null, null, null, null, null];
+                for (let i = 0; i < 5; i++) {
+                    if (i < 5 + 4 - self.data.damage) {
+                        self.data.turn.splice(i, 1, null);
+                    }
+                }
             }).catch(function(error) {
                 throw error;
             });
@@ -97,21 +101,46 @@ const Turn = new Vue({
     data: {
         'context': CONTROLLER.data,
     },
+    computed: {
+        'locked': function() {
+            if (this.context.damage < 4) {
+                return -1;
+            }
+            // slots=5 + locking offset=4
+            return 5 + 4 - this.context.damage;
+        }
+    },
     methods: {
         add: function(event) {
-            let curpos = event.newIndex + 1;
-            if (curpos == this.context.turn.length) {
-                curpos -= 2;
-            }
-            const current = this.context.turn.splice(curpos, 1)[0];
-            if (current) {
-                this.context.draw.push(current);
+            if (this.locked >= 0 && event.newIndex >= this.locked) {
+                // Unfortunately, this.move() does not trigger, so we have to
+                // "cancel" the drop ourselves.
+                const dropped = this.context.turn.splice(event.newIndex, 1)[0];
+                this.context.draw.push(dropped);
+            } else {
+              let curpos = event.newIndex + 1;
+              if (curpos == this.context.turn.length) {
+                  curpos -= 2;
+              }
+              const current = this.context.turn.splice(curpos, 1)[0];
+              if (current) {
+                  this.context.draw.push(current);
+              }
             }
         },
         remove: function(event) {
             if (this.context.turn.length < 5) {
                 this.context.turn.splice(event.oldIndex, 0, null);
             }
+        },
+        move: function(event) {
+            if (this.locked < 0) {
+                return true;
+            }
+            const source = event.draggedContext.index;
+            const target = event.draggedContext.futureIndex;
+            // return false to cancel drop
+            return source < this.locked && target < this.locked;
         }
     }
 });
